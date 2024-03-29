de0d6b4cff3a5d4cd21718a3485ead478d9edddb
Anshumohan Acharya <anshumohanacharya19@gmail.com>
2024-03-29 19:51:03 +0530

    server docker

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   const theme = useTheme();
